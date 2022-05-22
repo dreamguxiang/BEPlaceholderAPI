1ce4272b65aa815c733bb6b48733e6a7b1107933
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2022-05-22 23:47:42 +0800

    Update Command.cpp

--- BEPlaceholderAPI/Command.cpp
+++ BEPlaceholderAPI/Command.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Global.h"
 #include <DynamicCommandAPI.h>
 #include "PlaceholderAPI.h"
+#include <ScheduleAPI.h>
 
 void RegCommand()
 {
@@ -9,15 +10,23 @@ void RegCommand()
     command->setAlias("papi");
     auto& PAPIEnumList = command->setEnum("PAPIList", { "list" });
     auto& PAPIEnumInfo = command->setEnum("PAPIInfo", { "info","unregister" });
+
+    command->mandatory("PAPINameEnum", ParamType::SoftEnum,command->setSoftEnum("PAPINameList", {}));
+
     command->mandatory("PAPIEnum", ParamType::Enum, PAPIEnumList, CommandParameterOption::EnumAutocompleteExpansion);
     command->mandatory("PAPIEnum", ParamType::Enum, PAPIEnumInfo, CommandParameterOption::EnumAutocompleteExpansion);
-    command->mandatory("PAPIName", ParamType::String);
-
+    command->mandatory("PAPIName", ParamType::String); 
+	
 
     command->addOverload({ PAPIEnumList });
-    command->addOverload({ PAPIEnumInfo,"PAPIName"});
+    command->addOverload({ PAPIEnumInfo, "PAPINameEnum" });
+	
     command->setCallback([](DynamicCommand const& command, CommandOrigin const& origin, CommandOutput& output, std::unordered_map<std::string, DynamicCommand::Result>& results) {
         auto action = results["PAPIEnum"].get<std::string>();
+		
+       // command.getInstance()->addSoftEnumValues("PAPINameList", PlaceholderAPI::getPAPIList());
+		
+        //command.getInstance()->addSoftEnumValues("EnumNameList", softEnumNames);
         string str = "";
         auto sp = origin.getPlayer();
         switch (do_hash(action.c_str()))
@@ -35,7 +44,7 @@ void RegCommand()
         }
         
         case do_hash("info"): {
-            auto PAPIName = results["PAPIName"].get<std::string>();
+            auto PAPIName = results["PAPINameEnum"].getEnumValue();
             auto list = PlaceholderAPI::getPAPIInfoList();
             str += u8"§gPAPI " + PAPIName + " Info§r\n";
             int i = 0;
@@ -49,7 +58,7 @@ void RegCommand()
         }
 
         case do_hash("unregister"): {
-            auto PAPIName = results["PAPIName"].get<std::string>();
+            auto PAPIName = results["PAPINameEnum"].getEnumValue();
             auto out = PlaceholderAPI::unRegisterPlaceholder(PAPIName);
             if (out) {
                 output.success("Success");
@@ -63,5 +72,11 @@ void RegCommand()
             break;
         }
         });
-    DynamicCommand::setup(std::move(command));
+    auto cmd = DynamicCommand::setup(std::move(command));
+    Schedule::repeat([cmd] {
+        vector<string> v;
+        auto sets = PlaceholderAPI::getPAPIList();
+        v.assign(sets.begin(), sets.end());
+        cmd->addSoftEnumValues("PAPINameList", v);
+        }, 200);
 }
