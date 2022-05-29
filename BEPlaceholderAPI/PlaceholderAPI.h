bb6b79198c08e34e2c65e13d3f1d2274dadfbef5
HuoHua <2351078777@qq.com>
2022-05-29 23:41:53 +0800

    修复了API导出的问题

--- BEPlaceholderAPI/PlaceholderAPI.h
+++ BEPlaceholderAPI/PlaceholderAPI.h
@@ -2,6 +2,13 @@
 #include "Global.h"
 #include <DynamicCommandAPI.h>
 #define REG_PLUGIN_NAME PLUGIN_NAME
+
+#ifdef PAPI_EXPORTS
+#define PAPI_API __declspec(dllexport)
+#else
+#define PAPI_API __declspec(dllimport)
+#endif
+
 class PlaceholderAPI {
 public:
 	string mValue;
@@ -20,32 +27,32 @@ public:
 	PlaceholderAPI(string Name,int UpdateInterval,bool AutoUpdate,bool mProcessPlayer, bool mProcessParameters ,string PluginName,std::function<string(class Player*)> callback, std::function<string(class Player*, std::unordered_map<string, string>)> CallbackWithParameters);
 	~PlaceholderAPI() { };
 	
-	static void registerPlaceholder(PlaceholderAPI a0);
-	static string getValue(string name,Player* sp);
-	static string getValue(string name);
-	static void translateString(string& a0, Player* sp = nullptr);
+	PAPI_API static void registerPlaceholder(PlaceholderAPI a0);
+	PAPI_API static string getValue(string name,Player* sp);
+	PAPI_API static string getValue(string name);
+	PAPI_API static void translateString(string& a0, Player* sp = nullptr);
 
-	static void registerStaticPlaceholder(string name, string value, string PluginName = "");
-	static void registerStaticPlaceholder(string name, string(*Func)(), string PluginName);
-	static void registerStaticPlaceholder(string name, std::function<string()> callback, string PluginName="");
-	static void registerStaticPlaceholder(string name, int UpdateInterval, string(*Func)(), string PluginName);
-	static void registerStaticPlaceholder(string name, int UpdateInterval, std::function<string()> callback, string PluginName = "");
+	PAPI_API static void registerStaticPlaceholder(string name, string value, string PluginName = "");
+	PAPI_API static void registerStaticPlaceholder(string name, string(*Func)(), string PluginName);
+	PAPI_API static void registerStaticPlaceholder(string name, std::function<string()> callback, string PluginName="");
+	PAPI_API static void registerStaticPlaceholder(string name, int UpdateInterval, string(*Func)(), string PluginName);
+	PAPI_API static void registerStaticPlaceholder(string name, int UpdateInterval, std::function<string()> callback, string PluginName = "");
 	
-	static void registerPlayerPlaceholder(string name, std::function<string(class Player*)> callback, string PluginName = "");
-	static void registerPlayerPlaceholder(string name, std::function<string(class Player*, std::unordered_map<string, string>)> callback, string PluginName = "");
+	PAPI_API static void registerPlayerPlaceholder(string name, std::function<string(class Player*)> callback, string PluginName = "");
+	PAPI_API static void registerPlayerPlaceholder(string name, std::function<string(class Player*, std::unordered_map<string, string>)> callback, string PluginName = "");
 
-	static void registerServerPlaceholder(string name, std::function<string()> callback, string PluginName = "");
-	static void registerServerPlaceholder(string name, std::function<string(std::unordered_map<string, string>)> callback, string PluginName = "");
+	PAPI_API static void registerServerPlaceholder(string name, std::function<string()> callback, string PluginName = "");
+	PAPI_API static void registerServerPlaceholder(string name, std::function<string(std::unordered_map<string, string>)> callback, string PluginName = "");
 	
-	static bool unRegisterPlaceholder(string name);
+	PAPI_API static bool unRegisterPlaceholder(string name);
 	
-	static void Update(PlaceholderAPI a1);
-	static std::unordered_set<string> getPAPIPluginsList();
-	static std::vector<PlaceholderAPI> getPAPIInfoList();
-	inline string getValue() {
+	PAPI_API static void Update(PlaceholderAPI a1);
+	PAPI_API static std::unordered_set<string> getPAPIPluginsList();
+	PAPI_API static std::vector<PlaceholderAPI> getPAPIInfoList();
+	PAPI_API inline string getValue() {
 		return mValue;
 	}
-	inline string getPluginName() {
+	PAPI_API inline string getPluginName() {
 		return mPluginName;
 	}
 };
