6443f1acdc0f5c19eae7ad7ae4dba16e400b22f2
HuoHua <2351078777@qq.com>
2022-05-29 21:03:33 +0800

    change version number

--- BEPlaceholderAPI/Version.h
+++ BEPlaceholderAPI/Version.h
@@ -10,7 +10,7 @@
 #define PLUGIN_AUTHOR               "QingYu"
 #define PLUGIN_VERSION_MAJOR        1
 #define PLUGIN_VERSION_MINOR        0
-#define PLUGIN_VERSION_REVISION     1
+#define PLUGIN_VERSION_REVISION     0
 #define PLUGIN_VERSION_BUILD        0
 #define PLUGIN_VERSION_STATUS       PLUGIN_VERSION_RELEASE
 
